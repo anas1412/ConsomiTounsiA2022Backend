72281dc3c651c1787f9df13b87bf5f33659c3a16
Hadj-Sessi-Heni <heni.hadjsessi@esprit.tn>
2022-04-22 14:40:27 +0100

    Token+SpringSecurity+Authorities

--- src/main/java/tn/esprit/Repository/PublicitéRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import tn.esprit.Entities.Publicité;
-
-public interface PublicitéRepository extends CrudRepository<Publicité, Long> {
-
-}
